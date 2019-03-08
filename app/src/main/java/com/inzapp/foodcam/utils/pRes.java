c120fc50c63fe2241915d1bfd8b723894d1a2e95
root <root@localhost.localdomain>
2019-03-08 18:48:40 +0900

    knn 단일 순위 백업

--- app/src/main/java/com/inzapp/foodcam/utils/pRes.java
+++ app/src/main/java/com/inzapp/foodcam/utils/pRes.java
@@ -12,7 +12,7 @@ import java.util.concurrent.ExecutorService;
  * 프로젝트 내부적으로 사용되는 값들을 모아둠
  */
 public abstract class pRes {
-    public static final String IP = "172.30.1.20"; // 서버아이피
+    public static final String IP = "172.30.125.167"; // 서버아이피
     public static final int PORT = 12332; // 서버포트
     public static final int IMAGE_COMPRESSION_VAL = 100; // 이미지 압축률 : 100에 가까울수록 원본 화질로 전송된다
     public static final int TIMEOUT_CONNECTION = 1000 * 10; // 이 시간동안 서버에 연결하지 못하면 연결을 중지
